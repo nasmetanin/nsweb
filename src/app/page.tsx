d286350db7f282e9b41c53fdbcb56d251411e518
nasmetanin <smet.n@hotmail.com>
2025-09-13 01:39:44 +0300

    Add Lottie component and Clarity tracking script
    - Implemented a new Lottie component using DotLottieReact for rendering animations.
    - Added a Clarity tracking script for user interaction analytics.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,47 @@
-import styles from '@/styles/main.module.css'
-import { Metadata } from 'next'
-import About from '@/components/about'
-import { Projects } from '@/components/projects'
+import styles from "@/styles/main.module.css";
+import { Metadata } from "next";
+import About from "@/components/about";
+import { Projects } from "@/components/projects";
+import { Lottie } from "@/components/lottie";
 
 export const metadata: Metadata = {
-  title: 'Nikita Smetanin - Software Engineer',
-  description: 'Creative full-stack engineer that brings your dream to reality. Websites, web apps, mobile apps, backend API services, and more.',
-  robots: 'index, follow',
+  title: "Nikita Smetanin - Software Engineer",
+  description:
+    "Creative full-stack engineer that brings your dream to reality. Websites, web apps, mobile apps, backend API services, and more.",
+  robots: "index, follow",
   openGraph: {
-    title: 'Nikita Smetanin - Software Engineer',
-    description: 'Creative full-stack engineer that brings your dream to reality. Websites, web apps, mobile apps, backend API services, and more.',
+    title: "Nikita Smetanin - Software Engineer",
+    description:
+      "Creative full-stack engineer that brings your dream to reality. Websites, web apps, mobile apps, backend API services, and more.",
     images: [
       {
-        url: 'https://nsweb.tech/projects/placeholder.png',
+        url: "https://nsweb.tech/projects/placeholder.png",
         width: 585,
         height: 380,
-        alt: 'Nikita Smetanin - Software Engineer'
-      }
+        alt: "Nikita Smetanin - Software Engineer",
+      },
     ],
-    url: '/',
-    siteName: 'Nikita Smetanin',
+    url: "/",
+    siteName: "Nikita Smetanin",
   },
-}
+};
 
 export default async function Home() {
-
   return (
     <main>
       <section className={styles.intro}>
         <div className={styles.intro_inner}>
           <h1>
-            I'm <span>Nikita</span>,<br />
-            A software engineer
+            I'm <span>Nikita</span>,<br />A software engineer
           </h1>
-          <h2>
-            and this what I've been working on:
-          </h2>
+          <h2>and this what I've been working on:</h2>
         </div>
+        <Lottie
+          src="/assets/deal_out_duck.json"
+          autoplay
+          loop
+          className={styles.intro_lottie}
+        />
       </section>
 
       <section id="projects">
@@ -47,5 +52,5 @@ export default async function Home() {
         <About />
       </section>
     </main>
-  )
+  );
 }
--- src/components/stack.tsx
+++ src/components/stack.tsx
@@ -1,29 +1,35 @@
-import styles from '@/styles/stack.module.css'
-import Image from 'next/image'
+import styles from "@/styles/stack.module.css";
+import Image from "next/image";
+import { Lottie } from "./lottie";
 
 type StackProps = {
-    stackItems: {
-        name: string,
-        logo: string
-    }[]
-}
+  stackItems: {
+    name: string;
+    logo: string;
+  }[];
+};
 
 export const Stack = (props: StackProps) => {
+  const { stackItems } = props;
 
-    const { stackItems } = props;
-
-    return (
-        <div className={styles.grid}>
-            {
-                stackItems.map((item, index) => {
-                    return (
-                        <div key={index} className={styles.item}>
-                            <Image src={item.logo} alt={item.name} width={30} height={30} />
-                            <p>{item.name}</p>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
-}
+  return (
+    <div className={styles.stack_container}>
+      <Lottie
+        src="/assets/stack_duck.json"
+        autoplay
+        loop
+        className={styles.stack_lottie}
+      />
+      <div className={styles.grid}>
+        {stackItems.map((item, index) => {
+          return (
+            <div key={index} className={styles.item}>
+              <Image src={item.logo} alt={item.name} width={30} height={30} />
+              <p>{item.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
