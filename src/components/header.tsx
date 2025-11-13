9d3fbfe34b30a38f4a83c30cf60df0a3f05c302d
nasmetanin <smet.n@hotmail.com>
2025-11-13 20:47:54 +0200

    refactor: Rearrange Header component structure and update CSS styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,24 +5,26 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Link className={styles.logo} href="/">
-        NS.
-      </Link>
-
-      <div className={styles.nav}>
-        <Link
-          href="https://github.com/nasmetanin"
-          target="_blank"
-          title="Github"
-        >
-          <Image
-            src="/img/github.svg"
-            className="social_icon"
-            alt="Github"
-            width={25}
-            height={25}
-          />
+      <div className={styles.header_inner}>
+        <Link className={styles.logo} href="/">
+          NS.
         </Link>
+
+        <div className={styles.nav}>
+          <Link
+            href="https://github.com/nasmetanin"
+            target="_blank"
+            title="Github"
+          >
+            <Image
+              src="/img/github.svg"
+              className="social_icon"
+              alt="Github"
+              width={25}
+              height={25}
+            />
+          </Link>
+        </div>
       </div>
     </header>
   );
